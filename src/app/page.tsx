b84429a3b44e05c0f9691de34f655016e2c42f72
Valderrama20 <jose23122009@gmai.com>
2023-06-06 16:29:38 -0300

    Primera parte de landing casi lista

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import style from "./page.module.css"
+
 
 import Nav from '@/components/nav/Nav'
 
 export default function Home() {
   return (
-     <main>
+     <main className={style.main}>
       <header><Nav/></header>
+      <div className={style.info}>
+        <h1>Películas y series<br />ilimitadas y mucho más</h1>
+        <h2>Disfruta donde quieras. Cancela cuando quieras.</h2>
+        <p>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix.</p>
+         <div className={style.container_buttons}>
+          <input type="text" name="" id="" />
+          <button>Comenzar</button>
+         </div>
+        
+      </div>
      </main>
   )
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -6,13 +6,9 @@ export default function Nav () {
         <nav className={style.nav}>
             <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="logo" />
             <div className={style.container_buttons}>
-                <div >
-                    <img src="" alt="en proceso" />
                     <select name="" id="">
                     <option value="Español">Español ▾</option>
                     </select>
-                </div>
-                
                 <button>Cerrar Sesion</button>
             </div>
         </nav>
